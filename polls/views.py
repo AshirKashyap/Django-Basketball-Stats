0d94055b12a778d503e5469a29c07c8757f21fa7
AshirKashyap <63086233+AshirKashyap@users.noreply.github.com>
2021-04-26 12:30:53 -0700

    Fixing Some Bugs
    I fixed some issues with the def get function for my index view. It was giving me a multi value dict key error which i fixed!

--- polls/views.py
+++ polls/views.py
@@ -9,9 +9,22 @@ from .models import NBATeam, NBAPlayer, Profile
 
 
 class BasketballPoints(View):
-    # The post and get methods automatically seperate the data accordingly
+    # The post and get methods automatically seperate the data accordingly\
+
+    points = NBAPlayer.objects.all()
+
+    # def post(self, request):
+    #     return render(request, 'polls/points.html', {})
+
     def post(self, request):
-        return render(request, 'polls/points.html', {})
+        if request.method == 'POST':
+            print(request.POST['totalPoints'])
+            context = {
+                'points': self.points,
+                'name': request.POST['totalPoints'],
+            }
+
+            return render(request, 'polls/points.html', context)
 
     def get(self, request):
         return render(request, 'polls/points.html', {})
@@ -38,66 +51,15 @@ class IndexView(View):
                 'name': request.POST['firstName'],
             }
 
-        return render(request, 'polls/index.html', context)
-
-
-
+            return render(request, 'polls/index.html', context)
 
 
     def get(self, request):
-        if request.method == 'GET':
-            print(request.GET['firstName'])
-            context = {
+         if request.method == 'GET':
+             print(request.GET.get('firstName'))
+             context = {
                 'players': self.players,
-                'name': request.GET['firstName'],
-            }
+                'name': request.GET.get('firstName')
+             }
 
-        return render(request, 'polls/index.html', context)
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# class index(View):
-#     def get(self, request):
-#         template = loader.get_template('polls/index.html')
-#         context = {}
-#         return HTTPResponse(template.render(context, request))
-#
-#
-# class basketballPoints(View):
-#     def get(self, request):
-#         template = loader.get_template('polls/points.html')
-#         context = {}
-#         return HTTPResponse(template.render(context, request))
-#
-# class basketballRebounds(View):
-#     def get(self, request):
-#         template = loader.get_template('polls/rebounds.html')
-#         context = {}
-#         return HTTPResponse(template.render(context, request))
-
-
-# Create your views here.
-
-
-# from .models import Question
-# # ...
-# def index(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
-#     return render(request, 'polls/detail.html', {'question': question})
+             return render(request, 'polls/index.html', context)
