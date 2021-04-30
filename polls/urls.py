476cb12a59f00e7a4d1b8cb889d14f5f4e3824eb
AshirKashyap <63086233+AshirKashyap@users.noreply.github.com>
2021-04-30 14:55:04 -0700

    Starting to implement the login/logout system
    This commit is a little big but I started to code the login/logout system!

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('points', views.BasketballPoints.as_view(), name='bbpoints'),
     path('rebounds', views.BasketballRebounds.as_view(), name='bbrebounds'),
+    path('login', views.Login.as_view(), name='bblogin')
 ]
 
-#urls to go to two different views
+#urls to go to three different views
--- polls/views.py
+++ polls/views.py
@@ -3,10 +3,50 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.views import View
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 from .models import NBATeam, NBAPlayer, Profile
 
+class Login(View):
+    def post(self, request):
+        if 'inputUsername' in request.POST.keys():
+            # IF so, try to authentircate
+            user = authenticate(username=request.POST['inputUsername'],
+                password=request.POST['inputPassword'])
+            if user is not None:
+                # IF success, then use the login function so the session persists.
+                login(request, user)
+            else:
+                pass
+                # Message for failed login.
+        # This tests if the form is the log *out* form
+        elif 'logout' in request.POST.keys():
+            # If so, don't need to check anything else, just kill the session.
+            logout(request)
+        # After we check the forms, set a flag for use in the template.
+        if request.user.is_authenticated:
+            loggedIn = True
+        else:
+            loggedIn = False
+        # Find the template
+        template = loader.get_template('posts/index.html')
 
+        # The home page will show *all* posts for now.
+        allPosts = Post.objects.order_by('-pubDate')
+        # The Post model only contains the username, so we go and fetch the
+        # first and last names from the User model and add that information.
+        for post in allPosts:
+            poster = post.userPosted
+            post.firstName = poster.first_name
+            post.lastName = poster.last_name
+        # Now all the data is ready to pass to the template so set up the context.
+        context = {
+            'allPosts': allPosts,
+            'loggedIn': loggedIn,
+            'user': request.user,
+            }
+        # And go!
+        return render(template.render(context, request))
 
 class BasketballPoints(View):
     # The post and get methods automatically seperate the data accordingly\
