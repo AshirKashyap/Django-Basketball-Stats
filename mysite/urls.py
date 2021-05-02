eb7231ddae00a3bf4789c50a685016360293be52
AshirKashyap <63086233+AshirKashyap@users.noreply.github.com>
2021-05-01 20:35:45 -0700

    Created the register page/form
    I created the register page and coded the form in html. I also added some things to the views.py for the log in/log out function.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from register import views as v
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-
+    path('register/', v.register, name="register"),
     path('', include('django.contrib.auth.urls')),
 ]
--- register/views.py
+++ register/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-class Register(response):
+def register(response):
     return render()
