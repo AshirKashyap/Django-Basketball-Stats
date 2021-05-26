e34e9d25b72a359403ca39e8513a59ad1c7df72f
AshirKashyap <63086233+AshirKashyap@users.noreply.github.com>
2021-05-25 20:26:36 -0700

    Finalizing login/logout structure and function
    I finalized the login and logout function and ensured that when a user is logged out, they can register and be authenticated!

--- register/views.py
+++ register/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-
+from django.contrib.auth import login, authenticate
 # Create your views here.
 
 def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
-        print("REGISTRATION WHOOO")
+        print("REGISTRATION")
 
         if form.is_valid():
             form.save() #save a user in the user's database
+            #now need to login here
 
-            return redirect("/polls/rebounds") # need to fix this and redirect to a useful page  ################################
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username = username,
+            password = password)
+
+            if user is not None:
+                login(response, user = user)
+
+            return redirect("../polls/base")
     else:
         form = RegisterForm()
-
-
 
     return render(response, "register/register.html", {"form":form})
