6d53dad772dbcecd0beb780e7c8d7c16332d4113
AshirKashyap <63086233+AshirKashyap@users.noreply.github.com>
2021-05-27 20:12:13 -0700

    Deleting Points/Rebounds for concision and added more comments
    I deleted the points and rebounds views, templates, and urls. I did this so that all the data could be stored in the index.html file where the user could see the points per game, rebounds per game, 2 pt %, 3 pt %, and games played. (I had to delete a lot of code which made this process more streamlined but makes the views look relatively simple.)

--- polls/urls.py
+++ polls/urls.py
@@ -5,9 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('points', views.BasketballPoints.as_view(), name='bbpoints'),
-    path('rebounds', views.BasketballRebounds.as_view(), name='bbrebounds'),
     path('base', views.BaseView.as_view(), name='base'),
     ]
 
-#urls to go to three different views
+#urls to go to two different views
--- polls/views.py
+++ polls/views.py
@@ -9,35 +9,32 @@ from django.contrib.auth import authenticate, login, logout
 from .models import NBATeam, NBAPlayer, Profile, NBAPlayer2
 # Importing the models
 
+# Side note: I had four views (BaseView, Index, Points and Rebounds)
+# However, for concision and because it made more sense, I deleted the Points and Rebounds
+# views, templates, and urls in order to make it all into the Index view
+# So, I had to delete a lot of code which made it more streamlined
+
 class BaseView(View):
-    # Purpose: This function works to display the base.html template and its data
+    # Purpose: This function works to display the base.html template
     # Parameters: The only parameter is View which is a built in view model from django
     # Return: There is a return that renders base.html (in other words loads that page)
-    points = NBAPlayer.objects.all()
-    print(points)
 
+    # The post and get methods automatically seperate the data accordingly
     def post(self, request):
-        if 'totalPoints' in request.POST.keys():
-            print(request.POST['totalPoints'])
-            context = {
-                'points': self.points,
-                'point': request.POST['totalPoints'],
-            }
+        context = {}
 
-            print(totalPoints)
-            return render(request, 'polls/points.html', context)
-
-        else:
-            return render(request, 'polls/points.html', context)
+        return render(request, 'polls/base.html', context)
+        # renders the polls/base.html page (which basically just loads that page)
 
     def get(self, request):
-        # print(request.GET.get('totalPoints'))
-        context = {
-            'points': self.points,
-            # never printing points through get function
-        }
-        f = open("data.txt", "r")
+        context = {}
 
+        return render(request, 'polls/base.html', context)
+        # renders the polls/base.html page (which basically just loads that page)
+
+
+        # f = open("data.txt", "r")
+        #
         #
         # for line in f:
         #     strSpl = line.split(",")
@@ -65,70 +62,6 @@ class BaseView(View):
         # This code reads in data from the data.txt file and seperates it using split
         # While looping through each line of data, it creates a new player with the appropriate data
 
-        return render(request, 'polls/base.html', context)
-
-class BasketballPoints(View):
-    # Purpose: This function works to display the points.html template and its data
-    # Parameters: The only parameter is View which is a built in view model from django
-    # Return: There is a return that renders points.html (in other words loads that page)
-
-    # The post and get methods automatically seperate the data accordingly
-    points = NBAPlayer.objects.all()
-
-    def post(self, request):
-        if request.method == 'POST':
-            print(request.POST['totalPoints'])
-            context = {
-                'points': self.points,
-                'point': request.POST['totalPoints'],
-            }
-
-            return render(request, 'polls/points.html', context)
-
-    def get(self, request):
-        if request.method == 'GET':
-            print(request.GET.get('totalPoints'))
-            context = {
-                'points': self.points,
-                # never printing points through get function
-            }
-
-            return render(request, 'polls/points.html', context)
-
-
-
-class BasketballRebounds(View):
-    # Purpose: This function works to display the rebounds.html template and its data
-    # Parameters: The only parameter is View which is a built in view model from django
-    # Return: There is a return that renders rebounds.html (in other words loads that page)
-
-    # The post and get methods automatically seperate the data accordingly
-
-    rebounds = NBAPlayer.objects.all()
-
-    def post(self, request):
-        if request.method == 'POST':
-            print(request.POST['totalRebounds'])
-            context = {
-                'rebounds': self.rebounds,
-                'rebound': request.POST['totalRebounds'],
-            }
-
-            return render(request, 'polls/rebounds.html', context)
-
-
-    def get(self, request):
-        if request.method == 'GET':
-            print(request.GET.get('totalRebounds'))
-            context = {
-                'rebounds': self.rebounds,
-                # never printing the rebounds from the get function
-            }
-
-            return render(request, 'polls/rebounds.html', context)
-
-
-
 class IndexView(View):
     # Purpose: This function works to display the index.html template and its data
     # Parameters: The only parameter is View which is a built in view model from django
@@ -137,38 +70,36 @@ class IndexView(View):
     # The post and get methods automatically seperate the data accordingly
     players = NBAPlayer.objects.all()
 
-
     def post(self, request):
+        foundPlayer = False
+        # Checking to see if it found the right player
+        for player in self.players:
+            if str(player.firstName) + " " + str(player.lastName) == str(request.POST['firstName']):
+                # This if statement checks to see if the first and last name match the full name of the player
+                # (the key firstName refers to the full name of the player)
+                selectedPlayer = player
+                selectedPlayer2 = NBAPlayer2.objects.get(player = selectedPlayer)
+                # selected player 2 is for the NBAPlayer2 objects
+                # -- a workaround because i cannot edit the NBAPlayer model
+                context = {
+                    'players': self.players,
+                    'selectedPlayer': selectedPlayer,
+                    'addedFields': selectedPlayer2,
+                }
+                foundPlayer = True
+                # If it finds the right player, sets foundPlayer to True
+        if not foundPlayer:
+            context = {}
+            # This prevents the program from crashing because it ensures that either way context is defined
 
-        if request.method == 'POST':
-            # print(request.POST['firstName'])
-
-
-
-
-            for player in self.players:
-                if str(player.firstName) + " " + str(player.lastName) == str(request.POST['firstName']):
-                    selectedPlayer = player
-                    selectedPlayer2 = NBAPlayer2.objects.get(player = selectedPlayer)
-            context = {
-                'players': self.players,
-                # 'name': request.POST['firstName'],
-                'selectedPlayer': selectedPlayer,
-                'addedFields': selectedPlayer2,
-
-
-            }
-
-            print(request.POST)
-            return render(request, 'polls/index.html', context)
+        return render(request, 'polls/index.html', context)
+        # renders the polls/index.html page (which basically just loads that page)
 
 
     def get(self, request):
-         if request.method == 'GET':
-             # print(request.GET.get('firstName'))
-             context = {
-                'players': self.players,
-                #never printing name from get function
-             }
+         context = {
+            'players': self.players,
+         }
 
-             return render(request, 'polls/index.html', context)
+         return render(request, 'polls/index.html', context)
+         # renders the polls/index.html page (which basically just loads that page)
